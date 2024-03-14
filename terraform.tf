c0f0079aa07209295ed321685be501535d5ac9cb
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-14 21:03:14 +0100

    pin versions
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- terraform.tf
+++ terraform.tf
@@ -5,5 +5,17 @@ terraform {
       version = "5.40.0"
       source  = "hashicorp/aws"
     }
+    utils = {
+      version = "1.7.1"
+      source = "cloudposse/utils"
+    }
+    helm = {
+      version = "2.9.0"
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      version = "2.18.1"
+      source = "hashicorp/kubernetes"
+    }
   }
 }
