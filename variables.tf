c0a2275051ac74aac68fb17ea216b69746b7a24b
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2024-03-22 20:04:37 +0100

    cert-manager
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- variables.tf
+++ variables.tf
@@ -17,3 +17,26 @@ variable "namespace" {
   type        = string
   description = "Namespace for Autoscaler release"
 }
+
+
+variable "cluster_endpoint" {
+  sensitive   = true
+  type        = string
+  description = "Endpoint of the cluster."
+}
+
+variable "cluster_ca" {
+  sensitive   = true
+  type        = string
+  description = "CA certificate of the cluster."
+}
+
+variable "k8s_exec_args" {
+  type        = list(string)
+  description = "Args for Kubernetes provider exec plugin. Example command ['eks', 'get-token', '--cluster-name', '{clusterName}}']"
+}
+
+variable "k8s_exec_command" {
+  type        = string
+  description = "Command name for Kubernetes provider exec plugin. Example - 'aws"
+}
